67189053fbebb2ecf4fa00715327466ad649b1c8
SM298011 <SM298011>
2025-06-22 00:50:31 +0100

    Added Temp animations/model
    there is a basic attack,
    I've set up for other attacks on the anim graph,
    running animation is set up in it as well,
    player hit detection just needs some more work.

--- Sean/Sean's Scripts/PlayerCollision.cs
+++ Sean/Sean's Scripts/PlayerCollision.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Hello World");
+        }
     }
 }
--- Sean/Sean's Scripts/PlayerController.cs
+++ Sean/Sean's Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,45 +6,64 @@ using UnityEngine.InputSystem;
 //[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Rigidbody rb;
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float rotationSpeed = 360f;
     private Vector3 input3D;
 
     public PlayerControls playerControls;
     private InputAction move;
+    private InputAction basicAtk;
     private CharacterController charController;
+    private int comboCount = 1;
+
+    Animator animator;
     // Start is called before the first frame update
     private void Awake()
     {
-        playerControls = new PlayerControls();   
+        playerControls = new PlayerControls();
+
+
     }
     private void OnEnable()
     {
         move = playerControls.Player.Move;
+        basicAtk = playerControls.Player.BasicAttack;
         move.Enable();
-        charController = GetComponent<CharacterController>();
+        basicAtk.Enable();
+        basicAtk.performed += BasicAttack;
+        move.performed += GatherInput;
+        move.canceled += GatherInput;
+        animator = gameObject.GetComponent<Animator>();
     }
     private void OnDisable()
     {
+        move.canceled -= GatherInput;
+        move.performed -= GatherInput;
         move.Disable();
+        basicAtk.Disable();
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
-        GatherInput();
         look();
         Move();
     }
-    public void GatherInput()
+    public void GatherInput(InputAction.CallbackContext context)
     {
         Vector2 input2D = move.ReadValue<Vector2>();
         input3D = new Vector3(input2D.x, 0, input2D.y);
     }
     private void Move()
     {
-        Vector3 moveDirection = transform.forward * moveSpeed * input3D.magnitude * Time.deltaTime;
-        charController.Move(moveDirection);
+        rb.velocity= transform.forward * moveSpeed * input3D.magnitude * Time.deltaTime;
+        
+        if (rb.velocity.magnitude > 0)
+            animator.SetBool("IsMoving", true);
+        else
+            animator.SetBool("IsMoving", false);
+
     }
     private void look()
     {
@@ -54,5 +74,12 @@ public class PlayerController : MonoBehaviour
         Quaternion rotation = Quaternion.LookRotation(multMatrix, Vector3.up);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
 
+    }
+    private void BasicAttack(InputAction.CallbackContext context)
+    {
+        if (comboCount >= 4)
+            comboCount = 1;
+        animator.SetTrigger("Attack" + comboCount);
+        
     }
 }
