217958ce7b80c9da513f6376fff414e91200512f
SM298011 <SM298011>
2025-06-22 19:38:46 +0100

    Added Attacks back to controller
    modified controller to work with rigid body instead of a character controller
    this also applies gravity

--- Sean/Sean's Scripts/PlayerController.cs
+++ Sean/Sean's Scripts/PlayerController.cs
@@ -1,18 +1,29 @@
+using System;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.TextCore.Text;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Collider swordCollider;
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float rotationSpeed = 360f;
-
+    [SerializeField] private float[] AttackResetTimes;
+    [SerializeField] private float[] comboResetTimes;
     private Vector3 moveDirection;
     private Vector3 input3D;
-
+    private Coroutine attackCooldownCoroutine;
+    private Coroutine comboResetCoroutine;
     public PlayerControls playerControls;
     private InputAction move;
-    private CharacterController charController;
-
+    private InputAction basicAtk;
+    private int comboCount = 1;
+    private bool canAttack = true;
+    Animator animator;
     private void Awake()
     {
         playerControls = new PlayerControls();
@@ -21,38 +32,47 @@ public class PlayerController : MonoBehaviour
     private void OnEnable()
     {
         move = playerControls.Player.Move;
+        basicAtk = playerControls.Player.BasicAttack;
         move.Enable();
-        charController = GetComponent<CharacterController>();
+        basicAtk.Enable();
+        move.performed += GatherInput;
+        move.canceled += GatherInput;
+        basicAtk.performed += BasicAttack;
+        animator = gameObject.GetComponent<Animator>();
     }
 
     private void OnDisable()
     {
         move.Disable();
+        basicAtk.Disable();
     }
 
     private void Update()
     {
-        GatherInput();
         Look();
         Move();
     }
 
-    private void GatherInput()
+    private void GatherInput(InputAction.CallbackContext context)
     {
         Vector2 input2D = move.ReadValue<Vector2>();
         Vector3 rawInput = new Vector3(input2D.x, 0, input2D.y);
 
         // Rotate input to align with isometric perspective (45 degrees)
         Matrix4x4 isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, -45, 0));
-        input3D = isoMatrix.MultiplyPoint3x4(rawInput); // isometric-aligned movement direction
+        input3D = isoMatrix.MultiplyPoint3x4(rawInput);
     }
 
     private void Move()
     {
-        if (input3D.magnitude < 0.1f) return;
 
-        moveDirection = input3D.normalized * moveSpeed * Time.deltaTime;
-        charController.Move(moveDirection);
+        Vector3 moveVelocity = input3D.normalized * moveSpeed;
+        rb.velocity = moveVelocity;
+
+        if (rb.velocity.magnitude > 0)
+            animator.SetBool("IsMoving", true);
+        else
+            animator.SetBool("IsMoving", false);
     }
 
     private void Look()
@@ -61,5 +81,49 @@ public class PlayerController : MonoBehaviour
 
         Quaternion targetRotation = Quaternion.LookRotation(input3D, Vector3.up);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+    private void BasicAttack(InputAction.CallbackContext context)
+    {
+        if (!canAttack)
+            return;
+
+        canAttack = false;
+        swordCollider.enabled = true;
+        if (comboCount >= 4)
+            comboCount = 1;
+        Debug.Log(comboCount);
+        animator.SetTrigger("Attack" + comboCount);
+
+        if (attackCooldownCoroutine != null)
+            StopCoroutine(attackCooldownCoroutine);
+        attackCooldownCoroutine = StartCoroutine(AttackCooldown(AttackResetTimes[comboCount - 1]));
+
+
+        if (comboResetCoroutine != null)
+            StopCoroutine(comboResetCoroutine);
+        comboResetCoroutine = StartCoroutine(ComboResetTimer(comboResetTimes[comboCount - 1]));
+
+
+        Invoke(nameof(DisableCollision), 0.7f);
+        comboCount++;
+
+    }
+
+    private void DisableCollision()
+    {
+        swordCollider.enabled = false;
+    }
+
+    private IEnumerator AttackCooldown(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        canAttack = true;
+    }
+
+    private IEnumerator ComboResetTimer(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        comboCount = 1;
+        Debug.Log("Combo reset due to inactivity");
     }
 }
