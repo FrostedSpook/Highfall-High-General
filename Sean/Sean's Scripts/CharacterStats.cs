9c4c2991320bafa8e5ea101362ef003c0c54cb7a
SM298011 <SM298011>
2025-07-03 15:33:02 +0100

    Added PopUp damage

--- Sean/Sean's Scripts/CharacterStats.cs
+++ Sean/Sean's Scripts/CharacterStats.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -10,12 +11,17 @@ public class CharacterStats : MonoBehaviour
     public int maxhealth;
     public bool isHit;
 
+    public GameObject dmgPop;
+    public Vector3 popUpOffset = new Vector3(0, 1, 0);
+
     //function to be called by other scripts
     public void DealDamage(CharacterStats target, int min, int max)
     {
         int amount = Random.Range(min, max);
         target.TakeDamage(amount);
-
+        Vector3 ranOffset = new Vector3(Random.Range(-0.25f, 0.25f), Random.Range(-0.25f, 0.25f), 0);
+        GameObject popUpRef = Instantiate(dmgPop, target.transform.position + popUpOffset + ranOffset, Quaternion.identity);
+        popUpRef.GetComponentInChildren<TMP_Text>().text = amount.ToString();
         isHit = true;
         Invoke(nameof(CanBeHit), 0.7f);
     }
